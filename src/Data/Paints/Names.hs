d810af6c10bd548c79933218f7d4f342fe470fc9
Evgeny Nerush <nerush@appl.sci-nnov.ru>
2017-10-19 14:24:43 +0300

    minimal working example

---
+++ src/Data/Paints/Names.hs
@@ -0,0 +1,47 @@
+module Data.Paints.Names where
+
+import Data.Colour.SRGB
+
+alizarinCrimson :: (Ord a, Floating a) => Colour a
+alizarinCrimson = sRGB24 227 38 54
+
+paynesGrey :: (Ord a, Floating a) => Colour a
+paynesGrey = sRGB24 83 104 120
+
+lapisLazuli :: (Ord a, Floating a) => Colour a
+lapisLazuli = sRGB24 38 97 156
+
+maize :: (Ord a, Floating a) => Colour a
+maize = sRGB24 242 198 73
+
+marigold :: (Ord a, Floating a) => Colour a
+marigold = sRGB24 234 162 33
+
+royalBlueWeb :: (Ord a, Floating a) => Colour a
+royalBlueWeb = sRGB24 65 105 225
+
+ashGrey :: (Ord a, Floating a) => Colour a
+ashGrey = sRGB24 178 190 181
+
+cinerous :: (Ord a, Floating a) => Colour a
+cinerous = sRGB24 152 129 123
+
+taupe :: (Ord a, Floating a) => Colour a
+taupe = sRGB24 72 60 50
+
+davysGrey :: (Ord a, Floating a) => Colour a
+davysGrey = sRGB24 85 85 85
+
+jet :: (Ord a, Floating a) => Colour a
+jet = sRGB24 52 52 52
+
+gris :: (Ord a, Floating a) => Colour a
+gris = sRGB24 152 152 152
+
+yellowOchre :: (Ord a, Floating a) => Colour a
+yellowOchre = sRGB24 221 153 34
+
+olivine :: (Ord a, Floating a) => Colour a
+-- R dark olive green 3
+--olivine = sRGB24 162 205 90
+olivine = sRGB24 137 178 67
